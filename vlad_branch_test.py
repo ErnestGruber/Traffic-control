8440dfd60930d921f324cd72e23eeccba899e706
biggvladek <zuevvlad55@gmail.com>
2021-10-20 23:30:49 +0300

    Dbl linked list + hash table

--- vlad_branch_test.py
+++ vlad_branch_test.py
@@ -1,21 +1,9 @@
-import weakref
-class SimpleCache:
-    def __init__(self, cacheSize):
-        self.cache = weakref.WeakValueDictionary()
-        self.cache_LRU = []
-        self.cacheSize = cacheSize
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+class DoublyLinkedList:
 
-    def __getitem__(self, key):
-        # no exception catching - let calling code handle them if any
-        value = self.cache[key]
-        return self.touchCache(key, value)
 
-    def __setitem__(self, key, value):
-        self.touchCache(key, value)
 
-    def touchCache(self, key, value):
-        self.cache[key] = value
-        if value in self.cache_LRU:
-            self.cache_LRU.remove(value)
-        self.cache_LRU = self.cache_LRU[-(self.cacheSize-1):] + [ value ]
-        return value
