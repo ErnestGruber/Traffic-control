899b577445c52745d80b47a882cfd355559266ef
biggvladek <zuevvlad55@gmail.com>
2021-10-22 00:11:23 +0300

    1_test

---
+++ LRUcache_test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, main
+from vlad_branch_test import LRUcache
+
+
+class LRUtest(TestCase):
+    def setUp(cls):
+        cls.p = LRUcache(3)
+        cls.p.put(1, 2)
+        cls.p.put(2, 3)
+        cls.p.put(3, 4)
+
+    def test_get(self):
+        self.assertEqual(self.p.get(1), 2)
+
+
+if __name__ == '__main__':
+    main()
+
