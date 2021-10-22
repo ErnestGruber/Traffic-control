2af0f5ac97fcc9ec4a9432e406b6ec3d436211df
danilakutuzov <danilakutuzov@gmail.com>
2021-10-22 19:57:44 +0300

    Попытки в API

---
+++ GetData.py
@@ -0,0 +1,17 @@
+import requests
+
+def Get(id):
+    response = requests.get(f'https://api.via-dolorosa.ru/rc/{id}/full_info')
+    return response
+
+class GetCurrentStatus():
+    def Get(self,request, id):
+        response = requests.get(f'https://api.via-dolorosa.ru/rc/{id}/status')
+        return response
+
+class PostCustomPhase():
+    def Post(self, request, id, body):
+        responce = requests.post(f'https://api.via-dolorosa.ru/rc/{id}/custom_phase_program', json=body)
+        return responce
+
+
