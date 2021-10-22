7981cca7d737b9a49f9c23a53a05269189ec4fd7
biggvladek <zuevvlad55@gmail.com>
2021-10-22 23:01:29 +0300

    oop

--- bin_text.py
+++ bin_text.py
@@ -1,43 +1,48 @@
-def binary(n):
-    b = ''
-    while n > 0:
-        b = str(n % 2) + b
-        n = n // 2
-    return b
-def ten(data):
-    number = 0
-    len_dat = len(data)
-    for i in range(0, len_dat):
-        number += int(data[i]) * (2**(len_dat - i -1))
-    return str(number)
-
-def go_to_bin (file_input:str, file_output:str):
-    Fin_1 = open(file_input)
-    Fin_2 = open(file_output, 'w')
-    while True:
-        line=Fin_1.readline()
-        if not line:
-            break
-        for i in line.split():
-            Fin_2.write(binary(int(i)))
-            Fin_2.write(' ')
-        Fin_2.write('\n')
-
-    Fin_2.close()
+class data:
+    def __init__(self,file_input,file_output):
+        self.file_input=file_input
+        self.file_output=file_output
 
 
-def go_to_ten (file_input:str, file_output:str):
-    Fin_1 = open(file_input)
-    Fin_2 = open(file_output, 'w')
-    while True:
-        line=Fin_1.readline()
-        if not line:
-            break
-        for i in line.split():
-            Fin_2.write(ten(i))
-            Fin_2.write(' ')
-        Fin_2.write('\n')
+    def go_to_bin (self):
+        def binary(n):
+            b = ''
+            while n > 0:
+                b = str(n % 2) + b
+                n = n // 2
+            return b
+        Fin_1 = open(self.file_input)
+        Fin_2 = open(self.file_output, 'w')
+        while True:
+            line=Fin_1.readline()
+            if not line:
+                break
+            for i in line.split():
+                Fin_2.write(binary(int(i)))
+                Fin_2.write(' ')
+            Fin_2.write('\n')
 
-    Fin_2.close()
+        Fin_2.close()
+
+    def go_to_ten (self):
+        def ten(data):
+            number = 0
+            len_dat = len(data)
+            for i in range(0, len_dat):
+                number += int(data[i]) * (2**(len_dat - i -1))
+            return str(number)
+        Fin_1 = open(self.file_input)
+        Fin_2 = open(self.file_output, 'w')
+        while True:
+            line=Fin_1.readline()
+            if not line:
+                break
+            for i in line.split():
+                Fin_2.write(ten(i))
+                Fin_2.write(' ')
+            Fin_2.write('\n')
+
+        Fin_2.close()
+
 
 
