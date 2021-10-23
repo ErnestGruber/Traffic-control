ad5b692b4e78a35fa14fb708d396a880ff83ab72
biggvladek <zuevvlad55@gmail.com>
2021-10-23 06:09:30 +0300

    new_bin_coding

--- bin_text.py
+++ bin_text.py
@@ -1,59 +1,64 @@
-import os
-class bin_data:
-    def __init__(self,file_input,file_output):
-        self.file_input=file_input
+class bin_file:
+    def __init__(self,dict,file_output):
+        self.dict=dict
         self.file_output=file_output
 
-    def go_to_bin (self):
-        def binary(n):
-            b = ''
-            while n > 0:
-                b = str(n % 2) + b
-                n = n // 2
-            return b
-        Fin_1 = open(self.file_input)
-        Fin_2 = open(self.file_output, 'w')
-        while True:
-            sq=''
-            line=Fin_1.readline()
-            if not line:
-                break
-            for i in line.split():
-                sq+=binary(int(i))+' '
-            sq=sq.strip()
-            Fin_2.write(sq)
-            Fin_2.write('\n')
+    def go_to_bin_file(self):
+        def coding(sq:str):
+            res=''
+            dict={'a':'01000001','b':'01000010','c':'01000011','d':'01000100','e':'01000101','f':'01000110','g':'01000111','h':'01001000','i':'01001001','j':'01001010','k':'01001011','l':'01001100','m':'01001101','o':'01001111','q':'01010001','r':'01010010','s':'01010011','t':'01010100','u':'01010101','v':'01010110','w':'01010111','x':'01011000','y':'01011001','z':'01011010','n':'01001110','p':'01010000','0':'00110000','1':'00110001','2':'00110010','3':'00110011','4':'00110100','5':'00110101','6':'00110110','7':'00110111','8':'00111000','9':'00111001',}
+            for i in sq:
+                res+=dict.get(i)
+            return res
+        F=open(self.file_output,'w')
+        for key, value in dict.items():
+            key_new=coding(key)
+            value_new=coding(value)
+            F.write(key_new)
+            F.write(' ')
+            F.write(value_new)
+            F.write('\n')
+        F.close()
+        self.dict=None
 
-        Fin_2.close()
-        Fin_1.close()
-        if os.path.isfile(self.file_input):
-            os.remove(self.file_input)
-        else:
-            print('Error')
-
-class ten_data:
-    def __init__(self,file_input,file_output):
-        self.file_input=file_input
+class bin_dict:
+    def __init__(self,file_output):
+        self.dict={}
         self.file_output=file_output
 
-    def go_to_ten (self):
-        def ten(data):
-            number = 0
-            len_dat = len(data)
-            for i in range(0, len_dat):
-                number += int(data[i]) * (2**(len_dat - i -1))
-            return str(number)
-        Fin_1 = open(self.file_input)
-        Fin_2 = open(self.file_output, 'w')
+    def go_to_bin_dict(self):
+        F=open(self.file_output)
         while True:
-            sq=''
-            line=Fin_1.readline()
+            line=F.readline().split()
             if not line:
                 break
-            for i in line.split():
-                sq+=ten(i)+' '
-            sq=sq.strip()
-            Fin_2.write(sq)
-            Fin_2.write('\n')
+            else:
+                key=line[0]
+                value=line[1]
+                self.dict[key]=value
+        return self.dict
 
-        Fin_2.close()
+
+class json_dict:
+    def __init__(self,dict):
+        self.dict = dict
+        self.new_dict = {}
+
+    def go_to_json_dict(self):
+        def encoding(sq:str):
+            dict={'01000001':'a','01000010':'b','01000011':'c','01000100':'d','01000101':'e','01000110':'f','01000111':'g','01001000':'h','01001001':'i','01001010':'j','01001011':'k','01001100':'l','01001101':'m','01001111':'o','01010001':'q','01010010':'r','01010011':'s','01010100':'t','01010101':'u','01010110':'v','01010111':'w','01011000':'x','01011001':'y','01011010':'z','01001110':'n','01010000':'p','00110000':'0','00110001':'1','00110010':'2','00110011':'3','00110100':'4','00110101':'5','00110110':'6','00110111':'7','00111000':'8','00111001':'9',}
+            res=''
+            sq_new=''
+            for i in sq:
+                if len(sq_new)!=8:
+                    sq_new+=i
+                else:
+                    res+=dict[sq_new]
+                    sq_new=''
+                    sq_new+=i
+            res+=dict[sq_new]
+            return res
+        for j in self.dict:
+            key,value=j,self.dict[j]
+            self.new_dict[encoding(key)]=encoding(value)
+        return self.new_dict
