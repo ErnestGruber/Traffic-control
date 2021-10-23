45011ee99fefb1ba53f8d0ce709bc452e553151e
danilakutuzov <danilakutuzov@gmail.com>
2021-10-23 12:52:34 +0300

    Почистил гит, скоипоновал код

--- Analys/firstanalys.py
+++ Analys/firstanalys.py
@@ -2,10 +2,12 @@ import pandas as pd
 from scipy import cluster
 from scipy.cluster.vq import kmeans
 from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
+import numpy as np
 
 class AnomalyFind(ABC):
     @abstractmethod
-    def find(self, data, param):
+    def find(data, param):
         dataFPO = pd.read_excel(f'{data}')
         dataFPO.head()
         intensive_raw = dataFPO[f'{param}'].values
@@ -13,9 +15,16 @@ class AnomalyFind(ABC):
         intensive_raw = intensive_raw.astype('float64')
         centroids, avg_distance = kmeans(intensive_raw, 4)
         groups, cdist = cluster.vq.vq(intensive_raw, centroids)
-        error = cdist.max()
+        plt.scatter(intensive_raw, np.arange(0, len(intensive_raw), 1), c=groups)
+        plt.xlabel(f'{param}')
+        plt.ylabel('Values')
+        plt.show()
+        error = intensive_raw.max()
         print(error)
 
+test = AnomalyFind.find('/Users/valerijkutuzov/Downloads/Пример данных с детекторов транспорта /rawdata/ДТ1_От_ул_Фёдора_Полетаева_к_Окскому_пр_ду.xls', 'Intensive')
+
+print(test)
 
 
 
